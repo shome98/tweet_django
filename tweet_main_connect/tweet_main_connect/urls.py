73bf318f4f13de7d87d957a7afb68284230741f1
Sowparna Shome <soupornoshome@gmail.com>
2025-03-15 20:37:10 +0530

    add tweet urls

--- tweet_main_connect/tweet_main_connect/urls.py
+++ tweet_main_connect/tweet_main_connect/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import home
@@ -24,8 +24,8 @@ from .views import home
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', home),
+    path('tweet/',include('tweet.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
