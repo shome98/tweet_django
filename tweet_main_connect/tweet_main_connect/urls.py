095a611fca1fb6ca6cdea76230535289a750ff5e
Sowparna Shome <soupornoshome@gmail.com>
2025-03-17 22:14:43 +0530

    auth urls add

--- tweet_main_connect/tweet_main_connect/urls.py
+++ tweet_main_connect/tweet_main_connect/urls.py
@@ -20,11 +20,13 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import home
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', home),
-    path('tweet/',include('tweet.urls'))
+    path('tweet/',include('tweet.urls')),
+    path('accounts/',include('django.contrib.auth.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
